937a0cf1727f2df2d8f002c8243c41aa1db8f91c
Eric Hiler <esh@dulead.com>
2015-07-14 16:13:06 -0400

    Display flash messages upon failing to save due to validation.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,12 @@ class PostsController < ApplicationController
    end
 
    def create
-     post = Post.new(params.require(:post).permit(:title, :link, :body))
-     if post.save
-     else redirect_to new_post_path
-     redirect_to posts_path
+     @post = Post.new(params.require(:post).permit(:title, :link, :body))
+     if @post.save
+       redirect_to posts_path
+     else
+      flash[:error] = @post.errors.full_messages
+      render :new
      end
    end
 end
